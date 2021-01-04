5ed75c4fb078405254d0820292bc611c3c8565c8
sgi <69767184+sgilavendano@users.noreply.github.com>
2021-01-04 12:34:46 -0500

    Update algo.py
    I added the fundamental components

--- algo.py
+++ algo.py
@@ -1,1 +1,9 @@
+import requests
+import quandl as q
+import pandas as pd
+import matplotlib.pyplot as plt
 
+q.ApiConfig.api_key = 'r6sJbbi5zmcb38jP4jJr'
+
+mydata = q.get("EIA/PET_RWTC_D")
+print(mydata)
